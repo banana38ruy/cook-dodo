979485d25d73d4ccaa364076ae9e2416f2b8f5bb
banana38ruy <banana38ruy@yahoo.co.jp>
2021-02-23 20:46:29 +0900

    更新

--- app/javascript/packs/add-memo-form.js
+++ app/javascript/packs/add-memo-form.js
@@ -1,7 +1,3 @@
-// $(document).ready(function() {
-//   $(".add_cook a").
-//     data("association-insertion-method", 'append').
-//     data("association-insertion-node", function(link){
-//       return link.closest('.row').next('.row').find('.add_cooks_form')
-//     });
-// });
+window.addEventListener('DOMContentLoaded',() =>{
+  console.log("uhe")
+})
