8cd982a96f2136e976188d100add0c0b26b26c86
banana38ruy <banana38ruy@yahoo.co.jp>
2021-02-24 22:46:16 +0900

    Revert "更新"
    This reverts commit 979485d25d73d4ccaa364076ae9e2416f2b8f5bb.

--- app/javascript/packs/add-memo-form.js
+++ app/javascript/packs/add-memo-form.js
@@ -1,3 +1,7 @@
-window.addEventListener('DOMContentLoaded',() =>{
-  console.log("uhe")
-})
+// $(document).ready(function() {
+//   $(".add_cook a").
+//     data("association-insertion-method", 'append').
+//     data("association-insertion-node", function(link){
+//       return link.closest('.row').next('.row').find('.add_cooks_form')
+//     });
+// });
